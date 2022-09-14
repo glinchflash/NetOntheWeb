7f188b5eca323ced4a0b4bde20d42ce8e40e519c
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-09-14 11:55:34 +0200

    Added pages for Students, Teachers and Classrooms. All of them showing a list of their respective objects.
    still trying to figure out how to link it all

--- SchoolTryout/Pages/HelloWeb.cshtml.cs
+++ SchoolTryout/Pages/HelloWeb.cshtml.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Students.Model;
 namespace HelloWorld.Pages
 {
     public class HelloWebModel : PageModel
     {
         public string Message { get; set; } = "In page model: ";
+        public Student Sicco = new Student(1, "Sicco", 1);
         public void OnGet()
         {
         }
